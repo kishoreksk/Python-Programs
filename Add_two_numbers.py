4f16c3a67e5e0177ac0df1cfc50de4757d7f2ff1
kishore KS <kishore.shobana@gmail.com>
2020-08-01 23:01:40 +0530

    Addition of Two numbers

---
+++ Add_two_numbers.py
@@ -0,0 +1,19 @@
+# Simple Addition
+
+num1 = 5.0
+num2 = 10
+
+sum = num1 + num2
+
+print("The sum of two numbers is:", sum)
+print("The sum of {0} and {1} is {2}". format(num1,num2,sum))
+
+# Addition using user input
+
+num3 = input("Enter first number: ")
+num4 = input("Enter Second number: ")
+
+sum1 = float(num3) + float(num4)
+
+print("The Sum of Two Numbers = ", sum1)
+print("The sum of {0} and {1} is {2}". format(num3,num4,sum1))
